d0b3aebd64ca1028ebeb3fa3913cf31d29bc54c8
glrh11 <glrh11@163.com>
2017-07-27 17:43:06 +0800

    提交代码

--- read-in-life-web/src/app/tools/tools-list/tools-list.component.ts
+++ read-in-life-web/src/app/tools/tools-list/tools-list.component.ts
@@ -15,22 +15,22 @@ export class ToolsListComponent implements OnInit {
     this.tools_list = [
       new Tool(
         '/a_r/tools/unix_time',
-        'http://o9hjg7h8u.bkt.clouddn.com/',
+        'http://o9hjg7h8u.bkt.clouddn.com/pic_coin_time.png',
         '时间戳转换'
       ),
       new Tool(
         '/a_r/tools/parse_json',
-        'http://o9hjg7h8u.bkt.clouddn.com/',
+        'http://o9hjg7h8u.bkt.clouddn.com/pic_coin_json.png',
         'JSON格式化'
       ),
       new Tool(
         '/a_r/tools/base64_tool',
-        'http://o9hjg7h8u.bkt.clouddn.com/',
+        'http://o9hjg7h8u.bkt.clouddn.com/pic_coin_b64.png',
         'base64工具'
       ),
       new Tool(
         '/a_r/tools/generate_image',
-        'http://o9hjg7h8u.bkt.clouddn.com/',
+        'http://o9hjg7h8u.bkt.clouddn.com/pic_coin_tu.png',
         '图片生成'
       ),
     ];
